b878087fe497103690c0f649b218ac080f291d03
Nemias Martins <134625505+NemarteGH@users.noreply.github.com>
2025-06-12 18:32:15 -0300

    Update

--- exercicios/ex006-paginas/tabuada/script.js
+++ exercicios/ex006-paginas/tabuada/script.js
@@ -1,15 +1,21 @@
 function verificar(){
 
-    // Definição de variáveis
-    let numero = window.document.getElementById('num')
-    let tab = window.document.getElementById('seltab')
-    let divres = window.document.getElementById('resultado2')
+    // Criação do select
+    let selec = document.createElement('select')
+    selec.setAttribute('name','tabuada')
+    selec.setAttribute('id','seltab')
+    selec.setAttribute('size','10')
 
     // Criação e configuração da imagem
     let imagem = document.createElement('img')
     imagem.setAttribute('src','professor.png')
     imagem.style.transform = 'scaleX(-1)'
 
+    // Definição de variáveis
+    let numero = window.document.getElementById('num')
+    let divres = window.document.getElementById('resultado')
+    divres.innerHTML = ''
+   
     // Validação simples
     if (numero.value.length == 0){
         window.alert('Insira um número para iniciar')
@@ -20,21 +26,19 @@ function verificar(){
     }
     
     else {
-        tab.innerHTML = '' // Limpa o select
-        divres.innerHTML = ''
 
         let n = Number(numero.value)
-        
-        window.document.getElementById('seltab').style.
-        display = 'inline-block'
+        divres.appendChild(selec)
 
         for(var c = 0; c != 10; c++){
+
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
-            tab.appendChild(item)
+            selec.appendChild(item)
+
         }
 
-        imagem.style.display = 'inline-block'
         divres.appendChild(imagem)
+
     }
 }
